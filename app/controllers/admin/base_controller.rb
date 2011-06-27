489531398f9185564fa125b93b28f580f63ec78f
Kyle Partridge <partkyle@gmail.com>
2011-06-26 19:24:16 -0700

    hide css for non-admin pages

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,6 @@
 module Admin
   class BaseController < ApplicationController
     http_basic_authenticate_with :name => "", :password => "admin"
+    before_filter :get_module_name
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def get_module_name
+     @module_name = self.class.to_s.split("::").first
+   end
 end
