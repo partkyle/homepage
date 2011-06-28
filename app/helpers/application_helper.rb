299f3c940933d1baa1a9bd57dce9f509b238366e
Kyle Partridge <partkyle@gmail.com>
2011-06-27 23:57:09 -0700

    enable tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def markdown(text)
-    options = [:autolink, :no_intraemphasis]
+    options = [:autolink, :no_intraemphasis. :tables]
     Redcarpet.new(text, *options).to_html.html_safe if text
   end
   def property(name)
